3f3b7c79f049021c835b5ea92f48c1c96a3a3b3b
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-25 15:54:36 +0200

    [add]: print in HTML all posts

---
+++ js/script.js
@@ -0,0 +1,100 @@
+const posts = [
+    {
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        authorName: "Phil Mangione",
+        authorImage: "https://unsplash.it/300/300?image=15",
+        likes: 80,
+        created: "2021-06-25"
+    },
+    {
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        authorName: "Sofia Perlari",
+        authorImage: "https://unsplash.it/300/300?image=10",
+        likes: 120,
+        created: "2021-09-03"
+    },
+    {
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        authorName: "Chiara Passaro",
+        authorImage: "https://unsplash.it/300/300?image=20",
+        likes: 78,
+        created: "2021-05-15"
+    },
+    {
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        authorName: "Luca Formicola",
+        authorImage: null,
+        likes: 56,
+        created: "2021-04-03"
+    },
+    {
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        authorName: "Alessandro Sainato",
+        authorImage: "https://unsplash.it/300/300?image=29",
+        likes: 95,
+        created: "2021-03-05"
+    }
+];
+
+
+// elementi
+const postsList = document.querySelector('.posts-list');
+
+
+// reset
+postsList.innerHTML = '';
+
+
+// stampo tutti i post con innerHTML
+posts.forEach((post) => {
+    postsList.innerHTML += 
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.authorImage}" alt="${post.authorName}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.authorName}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post.index}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+
+
+    ` 
+
+})
+
+
+
+
