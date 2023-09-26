2a7b3f1c575f75e9f4e365f7ae23e45ee065a372
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-26 17:41:09 +0200

    [edit]: Js create all function

--- js/script.js
+++ js/script.js
@@ -1,111 +1,176 @@
 const posts = [
     {
-        id: 1,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/300?image=171",
-        authorName: "Phil Mangione",
-        authorImage: "https://unsplash.it/300/300?image=15",
-        likes: 80,
-        created: "2021-06-25"
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
     },
     {
-        id: 2,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=112",
-        authorName: "Sofia Perlari",
-        authorImage: "https://unsplash.it/300/300?image=10",
-        likes: 120,
-        created: "2021-09-03"
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
     },
     {
-        id: 3,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=234",
-        authorName: "Chiara Passaro",
-        authorImage: "https://unsplash.it/300/300?image=20",
-        likes: 78,
-        created: "2021-05-15"
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
     },
     {
-        id: 4,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=24",
-        authorName: "Luca Formicola",
-        authorImage: null,
-        likes: 56,
-        created: "2021-04-03"
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
     },
     {
-        id: 5,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=534",
-        authorName: "Alessandro Sainato",
-        authorImage: "https://unsplash.it/300/300?image=29",
-        likes: 95,
-        created: "2021-03-05"
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
     }
 ];
 
 
+
+
 // elementi
 const postsList = document.querySelector('.posts-list');
+let likesArray = [];
+
 
 // reset
 postsList.innerHTML = '';
 
 
-// stampo tutti i post con innerHTML
-posts.forEach((post) => {
-    console.log(post)
-    postsList.innerHTML += 
-    `
+// stampo tutti i post richiamando la funzione che li crea singolarmente
+posts.forEach( post => postsList.innerHTML += createPost(post))
+
+
+// funzione per creare il post
+function createPost(post){
+    const {id, content, media, author, likes, created} = post;
+    return `
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src="${post.authorImage}" alt="${post.authorName}">                    
+                    ${author.image ? authorImage(author) : authorInitials(author)}                   
                 </div>
                 <div class="post-meta__data">
-                    <div class="post-meta__author">${post.authorName}</div>
-                    <div class="post-meta__time">4 mesi fa</div>
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${dateFormat(created)}</div>
                 </div>                    
             </div>
         </div>
-        <div class="post__text">${post.content}</div>
+        <div class="post__text">${content}</div>
         <div class="post__image">
-            <img src="${post.media}" alt="">
+            <img src="${media}" alt="${author.name}">
         </div>
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="${post.index}">
+                    <a class="like-button js-like-button ${isAlreadyLiked(id) ? 'like-button--liked' : ''}" href="#" data-postid="1">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
                 </div>
             </div> 
         </div>            
     </div>
 
-    ` 
-    
-});
+    `
+}
 
 
-const btnLike = document.querySelectorAll('.js-likes-button');
+// funzione per dichiarare se un post è gia liked
+function isAlreadyLiked(id){
+    return likesArray.includes(id);
+}
 
-btnLike.forEach((index) => {
-    index.addEventListener('click', function (){
-        this.class
-    })
-    
+
+// formattazione della data
+function dateFormat(dateStr){
+    console.log(dateStr)
+    return dateStr.split('-').reverse().join('/');
+};
+
+
+// funzione per mettere l'immagine dell'autore
+function authorImage(author){
+    const {name, image} = author
+    return `<img class="profile-pic" src="${image}"" alt="${name}"></img>`
+};
+
+
+
+// funzione per mettere le iniziali dell'autore
+function authorInitials(author){
+    const { name } = author;
+
+    const letters = name.split(' ').map( nameSplit => nameSplit[0]);
+    const initials = letters.join('');
+    return `<div class="profile-pic-default">
+                <span>${initials}</span>
+            </div>
+            `
+};
+
+
+// bottone per like + contatore likes
+const likeButtons = document.querySelectorAll('.js-like-button');
+const likeCounters = document.querySelectorAll('.js-likes-counter');
+
+likeButtons.forEach((btn, index) => {
+    btn._id = posts[index].id;
+    btn._index = index;
+    btn.addEventListener('click', handleLikeBtn)
 })
 
 
+function handleLikeBtn(event){
+    event.preventDefault();
+    this.classList.toggle('like-button--liked');
 
+    const postSelected = posts.find( post => post.id === this._id);
 
+    if(likesArray.includes(this._id)){
+        likesArray = likesArray.filter( likeId => likeId !== this._id);
+        postSelected.likes--;
+    } else{
+        postSelected.likes++;
+        likesArray.push(this._id)
 
+    }
 
+    likeCounters[this._index].innerText = postSelected.likes;
+
+};
